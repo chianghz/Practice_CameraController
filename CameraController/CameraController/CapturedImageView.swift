ad030211fd2f73b85ffdae630cdc1e6f5d908266
Kevin Chiang <kevin.chiang@silkrode.com.tw>
2021-10-25 14:49:22 +0800

    refactor(CapturedImageView): improve code readability

--- CameraController/CameraController/CapturedImageView.swift
+++ CameraController/CameraController/CapturedImageView.swift
@@ -7,27 +7,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class CapturedImageView : UIView {
-    //MARK:- Vars
-    var image : UIImage? {
-        didSet {
-            guard let image = image else {return}
-            imageView.image = image
-        }
-    }
-    
-    //MARK:- View Components
+
     let imageView : UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.layer.cornerRadius = 8
         imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-    
-    //MARK:- Init
+
+    var image : UIImage? {
+        didSet {
+            guard let image = image else { return }
+            imageView.image = image
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: .zero)
         setupView()
@@ -36,20 +34,16 @@ class CapturedImageView : UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    //MARK:- Setup
+
     func setupView(){
-        translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .white
         layer.cornerRadius = 10
+
         addSubview(imageView)
-        
-        NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 2),
-            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2),
-            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2),
-            imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2),
-        ])
+        imageView.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().offset(2)
+            $0.bottom.trailing.equalToSuperview().offset(-2)
+        }
     }
 }
     
